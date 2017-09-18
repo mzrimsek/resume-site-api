bc7451cea12527524055d371d4a434d7b4544f00
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-18 12:39:20 -0400

    move dbsets to top
    just a formatting thing :+1:

--- Integration.EntityFramework/Models/DatabaseContext.cs
+++ Integration.EntityFramework/Models/DatabaseContext.cs
@@ -4,13 +4,13 @@ namespace Integration.EntityFramework.Models
 {
     public class DatabaseContext : DbContext
     {
+        public DbSet<Work> Work { get; set; }
+        
         public DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options) {}
 
         protected override void OnModelCreating(ModelBuilder builder) 
         {
             base.OnModelCreating(builder);
         }
-
-        public DbSet<Work> Work { get; set; }
     }
 }
