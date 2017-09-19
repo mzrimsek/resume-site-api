843b71b8e9eace705248b4c04ee64fc613cbce5e
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-19 00:02:22 -0400

    Add migration to include enddate in Job table

---
+++ Integration.EntityFramework/Migrations/20170919040139_FixJobTable.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Integration.EntityFramework.Migrations
+{
+    public partial class FixJobTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "EndDate",
+                table: "Jobs",
+                type: "timestamp",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EndDate",
+                table: "Jobs");
+        }
+    }
+}
