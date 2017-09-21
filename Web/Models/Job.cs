cb9359813382300340ffcbf690548b54db58620f
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-21 18:33:43 -0400

    added validation tags

--- Web/Models/Job.cs
+++ Web/Models/Job.cs
@@ -1,13 +1,21 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.Models
 {
     public class Job
     {
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string StartDate { get; set; }
+        [Required]
         public string EndDate { get; set; }
     }
 }
