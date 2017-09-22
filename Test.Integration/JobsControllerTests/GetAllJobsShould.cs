b307afd1b9fc5a0b5654e41623134c0f471d77f1
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-22 15:35:37 -0400

    added simple test for GetAllJobs

---
+++ Test.Integration/JobsControllerTests/GetAllJobsShould.cs
@@ -0,0 +1,30 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using Web;
+
+namespace Test.Integration.JobsControllerTests
+{
+    [TestClass]
+    public class GetAllJobsShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeOk()
+        {
+            var response = _client.GetAsync("/api/jobs").Result;
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+    }
+}
