826b5c9658e5808bfb300dec6ffae694ffbcb60d
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-23 16:18:12 -0400

    removed annotations
    this model will be for displaying data only, never for sending it back to the server

--- Web/Models/JobViewModel.cs
+++ Web/Models/JobViewModel.cs
@@ -1,21 +1,13 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace Web.Models
 {
     public class JobViewModel
     {
         public int Id { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string City { get; set; }
-        [Required]
         public string State { get; set; }
-        [Required]
         public string Title { get; set; }
-        [Required]
         public string StartDate { get; set; }
-        [Required]
         public string EndDate { get; set; }
     }
 }
