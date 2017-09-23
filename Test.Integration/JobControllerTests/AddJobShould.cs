51d8d160a6268bb341927022ec0a74cd3af93038
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-23 19:31:22 -0400

    added some tests
    given that one fails when I expected it to pass means that I'm pretty sure the one that's passing might not be passing for the right reason.

---
+++ Test.Integration/JobControllerTests/AddJobShould.cs
@@ -0,0 +1,62 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using Web;
+using Web.Models;
+using Test.Integration.Helpers;
+
+namespace Test.Integration.JobControllerTests
+{
+    [TestClass]
+    public class AddJobShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void Setup()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeCreated_WhenGivenValidModel()
+        {
+            var model = new AddUpdateJobViewModel()
+            {
+                Name = "Some Company",
+                City = "San Francisco",
+                State = "CA",
+                Title = "Developer",
+                StartDate = "1/1/2017",
+                EndDate = "7/1/2017"
+            };
+            var requestContent = RequestHelper.GetContentFromObject(model);
+
+            var response = _client.PostAsync("api/job", requestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeBadRequest_WhenGivenInvalidModel()
+        {
+            var model = new AddUpdateJobViewModel()
+            {
+                City = "San Francisco",
+                State = "CA",
+                Title = "Developer",
+                StartDate = "1/1/2017",
+                EndDate = "7/1/2017"
+            };
+            var requestContent = RequestHelper.GetContentFromObject(model);
+
+            var response = _client.PostAsync("api/job", requestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
