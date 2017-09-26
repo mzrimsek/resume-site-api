b9c6a704bb3c48913e1358295af84ec9f61393ee
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-26 17:03:13 -0400

    added school view models

---
+++ Web/Models/SchoolModels/SchoolViewModel.cs
@@ -0,0 +1,14 @@
+namespace Web.Models.SchoolModels
+{
+    public class SchoolViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string Major { get; set; }
+        public string Degree { get; set; }
+        public string StartDate { get; set; }
+        public string EndDate { get; set; }
+    }
+}
