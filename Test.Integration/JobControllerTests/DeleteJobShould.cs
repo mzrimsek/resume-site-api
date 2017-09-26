050e8e153e746a70233ecbc5d8cf389cfc85b912
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-25 21:36:56 -0400

    added 404 test for job delete
    I'm using this as my cleanup function so the fact that my tests consistently pass is enough for me to be confident this is working properly - just wanted to test the error case

---
+++ Test.Integration/JobControllerTests/DeleteJobShould.cs
@@ -0,0 +1,32 @@
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using Test.Integration.Helpers;
+using Web;
+using Web.Models;
+
+namespace Test.Integration.JobControllerTests
+{
+    [TestClass]
+    public class DeleteJobShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
+            _client = _server.CreateClient();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeNotFound_WhenGivenInvalidId()
+        {
+            var response = _client.DeleteAsync("/api/job/1").Result;
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+    }
+}
