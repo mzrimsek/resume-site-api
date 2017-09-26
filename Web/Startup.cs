0796764f44f9b3ec1b5ca4e0f5b4dda32b705379
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-26 16:49:07 -0400

    reorder usings

--- Web/Startup.cs
+++ Web/Startup.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Core.Interfaces;
-using Integration.EntityFramework.Models;
-using Integration.EntityFramework.Repositories;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Core.Interfaces;
+using Integration.EntityFramework.Models;
+using Integration.EntityFramework.Repositories;
 
 namespace Web
 {
