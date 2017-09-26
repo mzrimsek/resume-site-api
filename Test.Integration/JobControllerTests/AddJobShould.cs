a67b2dec9bb665a2c224a4ef7d823ec63218f0d1
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-25 21:02:52 -0400

    added new test to verify content posted to database
    also am using the delete route to do database clean up - it feels weird, but it works and it's not too terrible...it's also kinda testing my api works? I'll look for better solutions but this doesn't make me hate myself more than usual

--- Test.Integration/JobControllerTests/AddJobShould.cs
+++ Test.Integration/JobControllerTests/AddJobShould.cs
@@ -3,9 +3,9 @@ using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Net;
 using System.Net.Http;
+using Test.Integration.Helpers;
 using Web;
 using Web.Models;
-using Test.Integration.Helpers;
 
 namespace Test.Integration.JobControllerTests
 {
@@ -14,12 +14,19 @@ namespace Test.Integration.JobControllerTests
     {
         private TestServer _server;
         private HttpClient _client;
+        private int _jobId;
 
         [TestInitialize]
         public void Setup()
         {
             _server = new TestServer(new WebHostBuilder().UseStartup<Startup>());
             _client = _server.CreateClient();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _client.DeleteAsync($"/api/job/${_jobId}");
         }
 
         [TestMethod]
@@ -37,6 +44,8 @@ namespace Test.Integration.JobControllerTests
             var requestContent = RequestHelper.GetContentFromObject(model);
 
             var response = _client.PostAsync("/api/job", requestContent).Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<JobViewModel>(response);
+            _jobId = serializedContent.Id;
 
             Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
         }
@@ -57,6 +66,32 @@ namespace Test.Integration.JobControllerTests
             var response = _client.PostAsync("/api/job", requestContent).Result;
 
             Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnCorrespondingViewModel()
+        {
+            var model = new AddUpdateJobViewModel()
+            {
+                Name = "Some Company",
+                City = "San Francisco",
+                State = "CA",
+                Title = "Developer",
+                StartDate = "1/1/2017",
+                EndDate = "7/1/2017"
+            };
+            var requestContent = RequestHelper.GetContentFromObject(model);
+
+            var response = _client.PostAsync("/api/job", requestContent).Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<JobViewModel>(response);
+            _jobId = serializedContent.Id;
+
+            Assert.AreEqual(model.Name, serializedContent.Name);
+            Assert.AreEqual(model.City, serializedContent.City);
+            Assert.AreEqual(model.State, serializedContent.State);
+            Assert.AreEqual(model.Title, serializedContent.Title);
+            Assert.AreEqual(model.StartDate, serializedContent.StartDate);
+            Assert.AreEqual(model.EndDate, serializedContent.EndDate);
         }
     }
 }
--- Test.Integration/JobControllerTests/GetAllJobsShould.cs
+++ Test.Integration/JobControllerTests/GetAllJobsShould.cs
@@ -1,9 +1,12 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
 using System.Net;
 using System.Net.Http;
+using Test.Integration.Helpers;
 using Web;
+using Web.Models;
 
 namespace Test.Integration.JobControllerTests
 {
@@ -25,6 +28,14 @@ namespace Test.Integration.JobControllerTests
         {
             var response = _client.GetAsync("/api/job").Result;
             Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnEmptyList()
+        {
+            var response = _client.GetAsync("/api/job").Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<List<JobViewModel>>(response);
+            Assert.AreEqual(0, serializedContent.Count);
         }
     }
 }
