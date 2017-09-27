9c78119373a5b350e749e9e9655c0320aad92039
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-27 12:46:36 -0400

    add update school tests

---
+++ Test.Integration/ControllerTests/SchoolControllerTests/UpdateSchoolShould.cs
@@ -0,0 +1,93 @@
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using Test.Integration.TestHelpers;
+using Web.Models.SchoolModels;
+
+namespace Test.Integration.ControllerTests.SchoolControllerTests
+{
+    [TestClass]
+    public class UpdateSchoolShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+        private int _schoolId;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            (_server, _client) = new TestSetupHelper().GetTestServerAndClient();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            var _ = _client.DeleteAsync($"{ControllerRouteEnum.SCHOOL}/{_schoolId}").Result;
+            _client.Dispose();
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeNotFound_WhenGivenInvalidId()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PutAsync($"{ControllerRouteEnum.SCHOOL}/1", requestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeBadRequest_WhenGivenInvalidModel()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var postRequestContent = RequestHelper.GetRequestContentFromObject(model);
+            var postResponse = _client.PostAsync($"{ControllerRouteEnum.SCHOOL}", postRequestContent).Result;
+            _schoolId = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(postResponse).Id;
+            model = TestObjectCreator.GetAddUpdateSchoolViewModel(null);
+            var putRequestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var putResponse = _client.PutAsync($"{ControllerRouteEnum.SCHOOL}/{_schoolId}", putRequestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, putResponse.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeOk_WhenGivenValidIdAndValidModel()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var postRequestContent = RequestHelper.GetRequestContentFromObject(model);
+            var postResponse = _client.PostAsync($"{ControllerRouteEnum.SCHOOL}", postRequestContent).Result;
+            _schoolId = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(postResponse).Id;
+            var putRequestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var putResponse = _client.PutAsync($"{ControllerRouteEnum.SCHOOL}/{_schoolId}", putRequestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.OK, putResponse.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnUpdatedViewModel()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var postRequestContent = RequestHelper.GetRequestContentFromObject(model);
+            var postResponse = _client.PostAsync($"{ControllerRouteEnum.SCHOOL}", postRequestContent).Result;
+            _schoolId = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(postResponse).Id;
+            model = TestObjectCreator.GetAddUpdateSchoolViewModel("A Different School");
+            var putRequestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var putResponse = _client.PutAsync($"{ControllerRouteEnum.SCHOOL}/{_schoolId}", putRequestContent).Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(putResponse);
+
+            Assert.AreEqual(model.Name, serializedContent.Name);
+            Assert.AreEqual(model.City, serializedContent.City);
+            Assert.AreEqual(model.State, serializedContent.State);
+            Assert.AreEqual(model.Major, serializedContent.Major);
+            Assert.AreEqual(model.Degree, serializedContent.Degree);
+            Assert.AreEqual(model.StartDate, serializedContent.StartDate);
+            Assert.AreEqual(model.EndDate, serializedContent.EndDate);
+        }
+    }
+}
