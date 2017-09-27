fe6bc9c6d3ef36e3fe0197998efcc863e7a62221
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-27 11:50:04 -0400

    working on tests

---
+++ Test.Integration/ControllerTests/SchoolControllerTests/AddSchoolShould.cs
@@ -0,0 +1,73 @@
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using Test.Integration.TestHelpers;
+using Web.Models.SchoolModels;
+
+namespace Test.Integration.ControllerTests.SchoolControllerTests
+{
+    [TestClass]
+    public class AddSchoolShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+        private int _schoolId;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            (_server, _client) = new TestSetupHelper().GetTestServerAndClient();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            var _ = _client.DeleteAsync($"/api/school/{_schoolId}").Result;
+            _client.Dispose();
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeCreated_WhenGivenValidModel()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync("/api/school", requestContent).Result;
+            _schoolId = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(response).Id;
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeBadRequest_WhenGivenInvalidModel()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel(null);
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync("/api/school", requestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnCorrectViewModel()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync("/api/school", requestContent).Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(response);
+            _schoolId = serializedContent.Id;
+
+            Assert.AreEqual(model.Name, serializedContent.Name);
+            Assert.AreEqual(model.City, serializedContent.City);
+            Assert.AreEqual(model.State, serializedContent.State);
+            Assert.AreEqual(model.Major, serializedContent.Major);
+            Assert.AreEqual(model.Degree, serializedContent.Degree);
+            Assert.AreEqual(model.StartDate, serializedContent.StartDate);
+            Assert.AreEqual(model.EndDate, serializedContent.EndDate);
+        }
+    }
+}
--- Test.Integration/TestHelpers/TestObjectCreator.cs
+++ Test.Integration/TestHelpers/TestObjectCreator.cs
@@ -1,14 +1,10 @@
 using Web.Models.JobModels;
+using Web.Models.SchoolModels;
 
 namespace Test.Integration.TestHelpers
 {
     public static class TestObjectCreator
     {
-        public static AddUpdateJobViewModel GetAddUpdateJobViewModel()
-        {
-            return GetAddUpdateJobViewModel("Some Company");
-        }
-
         public static AddUpdateJobViewModel GetAddUpdateJobViewModel(string name)
         {
             return new AddUpdateJobViewModel()
@@ -20,6 +16,30 @@ namespace Test.Integration.TestHelpers
                 StartDate = "1/1/2017",
                 EndDate = "7/1/2017"
             };
+        }
+
+        public static AddUpdateJobViewModel GetAddUpdateJobViewModel()
+        {
+            return GetAddUpdateJobViewModel("Some Company");
+        }
+
+        public static AddUpdateSchoolViewModel GetAddUpdateSchoolViewModel(string name)
+        {
+            return new AddUpdateSchoolViewModel()
+            {
+                Name = name,
+                City = "Kent",
+                State = "OH",
+                Major = "Computer Science",
+                Degree = "B.S.",
+                StartDate = "9/1/2015",
+                EndDate = "5/13/2017"
+            };
+        }
+
+        public static AddUpdateSchoolViewModel GetAddUpdateSchoolViewModel()
+        {
+            return GetAddUpdateSchoolViewModel("Kent State University");
         }
     }
 }
