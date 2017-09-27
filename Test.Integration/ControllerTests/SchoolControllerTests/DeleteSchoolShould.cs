924fb2269ac70eef85ea6e3d0888d99af982a7f8
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-27 12:32:40 -0400

    added delete tests

---
+++ Test.Integration/ControllerTests/SchoolControllerTests/DeleteSchoolShould.cs
@@ -0,0 +1,64 @@
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Net;
+using System.Net.Http;
+using Test.Integration.TestHelpers;
+using Web.Models.SchoolModels;
+
+namespace Test.Integration.ControllerTests.SchoolControllerTests
+{
+    [TestClass]
+    public class DeleteSchoolShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            (_server, _client) = new TestSetupHelper().GetTestServerAndClient();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            _client.Dispose();
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeNotFound_WhenGivenInvalidId()
+        {
+            var response = _client.DeleteAsync($"{ControllerRouteEnum.SCHOOL}/1").Result;
+            Assert.AreEqual(HttpStatusCode.NotFound, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeNoContent_WhenGivenValidId()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var postRequestContent = RequestHelper.GetRequestContentFromObject(model);
+            var postResponse = _client.PostAsync($"{ControllerRouteEnum.SCHOOL}", postRequestContent).Result;
+            var schoolId = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(postResponse).Id;
+
+            var deleteReponse = _client.DeleteAsync($"{ControllerRouteEnum.SCHOOL}/{schoolId}").Result;
+
+            Assert.AreEqual(HttpStatusCode.NoContent, deleteReponse.StatusCode);
+        }
+
+        [TestMethod]
+        public void DeleteSchool()
+        {
+            var model = TestObjectCreator.GetAddUpdateSchoolViewModel();
+            var postRequestContent = RequestHelper.GetRequestContentFromObject(model);
+            var postResponse = _client.PostAsync("/api/school", postRequestContent).Result;
+            var schoolId = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(postResponse).Id;
+            var _ = _client.DeleteAsync($"/api/school/{schoolId}").Result;
+
+            var getResponse = _client.GetAsync($"/api/school/${schoolId}").Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<SchoolViewModel>(getResponse);
+
+            Assert.AreEqual(HttpStatusCode.NotFound, getResponse.StatusCode);
+        }
+    }
+}
