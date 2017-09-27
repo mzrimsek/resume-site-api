65c1297a821c2a1715a984dfc1f0ea764828c85a
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-27 15:22:09 -0400

    add job project route enum

--- Test.Integration/ControllerTests/ControllerRouteEnum.cs
+++ Test.Integration/ControllerTests/ControllerRouteEnum.cs
@@ -4,6 +4,7 @@ namespace Test.Integration.ControllerTests
     {
         public static string JOB = new ControllerRouteEnum("job").Route;
         public static string SCHOOL = new ControllerRouteEnum("school").Route;
+        public static string JOB_PROJECT = new ControllerRouteEnum("jobProject").Route;
 
         public string Route { get; private set; }
         public ControllerRouteEnum(string route)
