ad4f5e45bb7d48694c752a90d7ddf464ef4c8600
Mike Zrimsek <mikezrimsek@gmail.com>
2017-09-29 01:31:07 -0400

    add jobid validation
    job id must be greater than 0 to be valid

--- Web/Models/JobProjectModels/AddUpdateJobProjectViewModel.cs
+++ Web/Models/JobProjectModels/AddUpdateJobProjectViewModel.cs
@@ -5,6 +5,7 @@ namespace Web.Models.JobProjectModels
     public class AddUpdateJobProjectViewModel
     {
         [Required]
+        [Range(1, double.PositiveInfinity, ErrorMessage = "Invalid Job ID")]
         public int JobId { get; set; }
         [Required]
         public string Name { get; set; }
