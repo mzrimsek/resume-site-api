fbad1069ba3ea2648de2802202433f09a77d772e
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-02 11:13:28 -0400

    add view models

--- Web/Models/LanguageModels/AddUpdateLanguageViewModel.cs
+++ Web/Models/LanguageModels/AddUpdateLanguageViewModel.cs
@@ -1,7 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.Models.LanguageModels
 {
     public class AddUpdateLanguageViewModel
     {
-        
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 3, ErrorMessage = "Invalid Rating - Must Be 1, 2, or 3")]
+        public int Rating { get; set; }
     }
 }
--- Web/Models/LanguageModels/LanguageViewModel.cs
+++ Web/Models/LanguageModels/LanguageViewModel.cs
@@ -2,6 +2,9 @@ namespace Web.Models.LanguageModels
 {
     public class LanguageViewModel
     {
-        
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+        public string RatingName { get; set; }
     }
 }
