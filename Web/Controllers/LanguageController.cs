74913e9867cf175a198a72f70732d59398de222a
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-02 11:31:34 -0400

    added controller routes

--- Web/Controllers/LanguageController.cs
+++ Web/Controllers/LanguageController.cs
@@ -18,31 +18,69 @@ namespace Web.Controllers
         [HttpGet]
         public IActionResult GetAllLanguages()
         {
-            return new EmptyResult();
+            var languages = _languageRepository.GetAll();
+            var languageViews = LanguageViewModelMapper.MapFrom(languages);
+            return Ok(languageViews);
         }
 
         [HttpGet("{id}", Name = "GetLanguage")]
         public IActionResult GetLanguage(int id)
         {
-            return new EmptyResult();
+            var language = _languageRepository.GetById(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+            var languageViewModel = LanguageViewModelMapper.MapFrom(language);
+            return Ok(languageViewModel);
         }
 
         [HttpPost]
         public IActionResult AddLanguage([FromBody] AddUpdateLanguageViewModel language)
         {
-            return new EmptyResult();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var domainModel = LanguageDomainModelMapper.MapFrom(language);
+            var savedLanguage = _languageRepository.Save(domainModel);
+            var languageViewModel = LanguageViewModelMapper.MapFrom(savedLanguage);
+
+            return CreatedAtRoute("GetLanguage", new { id = languageViewModel.Id }, languageViewModel);
         }
 
         [HttpPut("{id}")]
-        public IActionResult UpdateLanguage(int id, [FromBody] AddUpdateLanguageViewModel languageViewModel)
+        public IActionResult UpdateLanguage(int id, [FromBody] AddUpdateLanguageViewModel language)
         {
-            return new EmptyResult();
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var foundLanguage = _languageRepository.GetById(id);
+            if (foundLanguage == null)
+            {
+                return NotFound();
+            }
+
+            var viewModel = LanguageViewModelMapper.MapFrom(id, language);
+            var domainModel = LanguageDomainModelMapper.MapFrom(viewModel);
+            var updatedDomainModel = _languageRepository.Save(domainModel);
+
+            var updatedViewModel = LanguageViewModelMapper.MapFrom(updatedDomainModel);
+            return Ok(updatedViewModel);
         }
 
         [HttpDelete("{id}")]
         public IActionResult DeleteLanguage(int id)
         {
-            return new EmptyResult();
+            var language = _languageRepository.GetById(id);
+            if (language == null)
+            {
+                return NotFound();
+            }
+
+            _languageRepository.Delete(id);
+            return NoContent();
         }
     }
 }
