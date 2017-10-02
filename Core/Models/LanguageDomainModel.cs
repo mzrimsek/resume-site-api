2624949152b59186dacc9943e4310741ba47178b
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-02 00:47:56 -0400

    add language domain model

---
+++ Core/Models/LanguageDomainModel.cs
@@ -0,0 +1,9 @@
+namespace Core.Models
+{
+    public class LanguageDomainModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Rating { get; set; }
+    }
+}
