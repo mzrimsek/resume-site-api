05747d47ee1234bcb5f6c53f227ede57f5746179
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-02 11:13:36 -0400

    add mappers

--- Web/Mappers/LanguageMappers/LanguageDomainModelMapper.cs
+++ Web/Mappers/LanguageMappers/LanguageDomainModelMapper.cs
@@ -1,7 +1,36 @@
+using System.Collections.Generic;
+using Core.Models;
+using Web.Models.LanguageModels;
+
 namespace Web.Mappers.LanguageMappers
 {
     public static class LanguageDomainModelMapper
     {
+        public static LanguageDomainModel MapFrom(LanguageViewModel viewModel)
+        {
+            return new LanguageDomainModel()
+            {
+                Id = viewModel.Id,
+                Name = viewModel.Name,
+                Rating = viewModel.Rating
+            };
+        }
 
+        public static IEnumerable<LanguageDomainModel> MapFrom(IEnumerable<LanguageViewModel> viewModels)
+        {
+            foreach (var viewModel in viewModels)
+            {
+                yield return MapFrom(viewModel);
+            }
+        }
+
+        public static LanguageDomainModel MapFrom(AddUpdateLanguageViewModel viewModel)
+        {
+            return new LanguageDomainModel()
+            {
+                Name = viewModel.Name,
+                Rating = viewModel.Rating
+            };
+        }
     }
 }
