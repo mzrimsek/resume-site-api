f265a45859eda3efc87bcf73291eded066e0e069
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-02 12:39:32 -0400

    working on tests

--- Test.Integration/ControllerTests/LanguageControllerTests/AddLanguageShould.cs
+++ Test.Integration/ControllerTests/LanguageControllerTests/AddLanguageShould.cs
@@ -33,31 +33,64 @@ namespace Test.Integration.ControllerTests.LanguageControllerTests
         [TestMethod]
         public void ReturnStatusCodeCreated_WhenGivenValidModel()
         {
-            Assert.Fail();
+            var model = TestObjectGetter.GetAddUpdateLanguageViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.LANGUAGE}", requestContent).Result;
+            _languageId = RequestHelper.GetObjectFromResponseContent<LanguageViewModel>(response).Id;
+
+            Assert.AreEqual(HttpStatusCode.Created, response.StatusCode);
         }
 
         [TestMethod]
         public void ReturnStatusCodeBadRequest_WhenGivenInvalidModel_WithInvalidName()
         {
-            Assert.Fail();
+            var model = TestObjectGetter.GetAddUpdateLanguageViewModel(null, 1);
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.LANGUAGE}", requestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
         [TestMethod]
         public void ReturnStatusCodeBadRequest_WhenGivenInvalidModel_WithInvalidRating()
         {
-            Assert.Fail();
+            var model = TestObjectGetter.GetAddUpdateLanguageViewModel("Java", 0);
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.LANGUAGE}", requestContent).Result;
+
+            Assert.AreEqual(HttpStatusCode.BadRequest, response.StatusCode);
         }
 
         [TestMethod]
         public void ReturnCorrectViewModel()
         {
-            Assert.Fail();
+            var model = TestObjectGetter.GetAddUpdateLanguageViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.LANGUAGE}", requestContent).Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<LanguageViewModel>(response);
+            _languageId = serializedContent.Id;
+
+            var isCorrectViewModel = AssertHelper.AreLanguageViewModelsEqual(model, serializedContent);
+            Assert.IsTrue(isCorrectViewModel);
         }
 
         [TestMethod]
         public void SaveCorrectViewModel()
         {
-            Assert.Fail();
+            var model = TestObjectGetter.GetAddUpdateLanguageViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.LANGUAGE}", requestContent).Result;
+            _languageId = RequestHelper.GetObjectFromResponseContent<LanguageViewModel>(response).Id;
+            response = _client.GetAsync($"{ControllerRouteEnum.LANGUAGE}/{_languageId}").Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<LanguageViewModel>(response);
+
+            var isCorrectViewModel = AssertHelper.AreLanguageViewModelsEqual(model, serializedContent);
+            Assert.IsTrue(isCorrectViewModel);
         }
     }
 }
