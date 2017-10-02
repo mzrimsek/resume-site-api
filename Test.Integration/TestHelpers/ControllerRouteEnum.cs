29e7956da598c6885618cfe6e1c69691fad09e13
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-02 11:40:02 -0400

    added route enum for language controller

--- Test.Integration/TestHelpers/ControllerRouteEnum.cs
+++ Test.Integration/TestHelpers/ControllerRouteEnum.cs
@@ -5,6 +5,7 @@ namespace Test.Integration.TestHelpers
         public static string JOB = new ControllerRouteEnum("job").Route;
         public static string SCHOOL = new ControllerRouteEnum("school").Route;
         public static string JOB_PROJECT = new ControllerRouteEnum("jobProject").Route;
+        public static string LANGUAGE = new ControllerRouteEnum("language").Route;
 
         public string Route { get; private set; }
         public ControllerRouteEnum(string route)
