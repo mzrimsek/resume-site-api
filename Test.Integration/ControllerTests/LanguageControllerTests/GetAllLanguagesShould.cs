9aa1ec8926e7dc15c416e91af0f6d99e42d7887f
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-03 01:23:24 -0400

    add get all languages tests

---
+++ Test.Integration/ControllerTests/LanguageControllerTests/GetAllLanguagesShould.cs
@@ -0,0 +1,61 @@
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using Test.Integration.TestHelpers;
+using Web.Models.LanguageModels;
+
+namespace Test.Integration.ControllerTests.LanguageControllerTests
+{
+    [TestClass]
+    public class GetAllLanguagesShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+        private TestObjectCreator _testObjectCreator;
+        private int _languageId;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            (_server, _client) = new TestSetupHelper().GetTestServerAndClient();
+            _testObjectCreator = new TestObjectCreator(_client);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            var _ = _client.DeleteAsync($"{ControllerRouteEnum.LANGUAGE}/{_languageId}").Result;
+            _client.Dispose();
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeOk()
+        {
+            var response = _client.GetAsync($"{ControllerRouteEnum.LANGUAGE}").Result;
+            Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [TestMethod]
+        public void ReturnEmptyList_WhenNoLanguagesAreCreated()
+        {
+            var response = _client.GetAsync($"{ControllerRouteEnum.LANGUAGE}").Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<List<LanguageViewModel>>(response);
+            Assert.AreEqual(0, serializedContent.Count);
+        }
+
+        [TestMethod]
+        public void ReturnOneJob_WhenOneLanguageIsCreated()
+        {
+            _languageId = _testObjectCreator.GetIdFromNewLanguage();
+
+            var response = _client.GetAsync($"{ControllerRouteEnum.LANGUAGE}").Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<List<LanguageViewModel>>(response);
+
+            Assert.AreEqual(1, serializedContent.Count);
+            Assert.AreEqual(_languageId, serializedContent[0].Id);
+        }
+    }
+}
