63f725459856b1208569577988c76f47ef6f2f15
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-03 12:26:01 -0400

    added data annotations to view model

--- Web/Models/JobModels/JobViewModel.cs
+++ Web/Models/JobModels/JobViewModel.cs
@@ -1,12 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Web.Models.JobModels
 {
     public class JobViewModel
     {
+        [Required]
         public int Id { get; set; }
+        [Required]
         public string Name { get; set; }
+        [Required]
         public string City { get; set; }
+        [Required]
         public string State { get; set; }
+        [Required]
         public string Title { get; set; }
+        [Required]
         public string StartDate { get; set; }
         public string EndDate { get; set; }
     }
