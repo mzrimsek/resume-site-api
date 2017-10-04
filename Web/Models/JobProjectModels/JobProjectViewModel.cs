520fc3f6383176307e158b4c636b61208d00a2ec
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-03 20:06:37 -0400

    removed annotations
    normal view models are purely presentational

--- Web/Models/JobProjectModels/JobProjectViewModel.cs
+++ Web/Models/JobProjectModels/JobProjectViewModel.cs
@@ -4,14 +4,9 @@ namespace Web.Models.JobProjectModels
 {
     public class JobProjectViewModel
     {
-        [Required]
         public int Id { get; set; }
-        [Required]
-        [Range(1, double.PositiveInfinity, ErrorMessage = "Invalid Job ID")]
         public int JobId { get; set; }
-        [Required]
         public string Name { get; set; }
-        [Required]
         public string Description { get; set; }
     }
 }
