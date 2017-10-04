eeb366a938cd4a5d64ecd590b55d6aaad9a4b440
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-04 10:25:13 -0400

    renamed test

--- Test.Unit/HelperTests/DateHelperTests/FormatShould.cs
+++ Test.Unit/HelperTests/DateHelperTests/FormatShould.cs
@@ -5,13 +5,13 @@ using Core.Helpers;
 namespace Test.Unit.HelperTests.DateHelperTests
 {
     [TestClass]
-    public class FormatDateShould
+    public class FormatShould
     {
         [TestMethod]
         public void ReturnCorrectlyFormattedDateString()
         {
             var date = new DateTime(2017, 7, 22);
-            var formattedDate = DateHelper.FormatDate(date);
+            var formattedDate = date.Format();
             Assert.AreEqual("7/22/2017", formattedDate);
         }
     }
