0f7e6d99ad935b782f5fca53f3f4cfab5fe405e0
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-06 12:59:32 -0400

    add skill database mapping profile

---
+++ Integration.EntityFramework/MapperProfiles/SkillProfile.cs
@@ -0,0 +1,15 @@
+using AutoMapper;
+using Core.Models;
+using Integration.EntityFramework.Models;
+
+namespace Integration.EntityFramework.MapperProfiles
+{
+    public class SkillProfile : Profile
+    {
+        public SkillProfile()
+        {
+            CreateMap<SkillDatabaseModel, SkillDomainModel>();
+            CreateMap<SkillDomainModel, SkillDatabaseModel>();
+        }
+    }
+}
