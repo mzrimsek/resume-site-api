073b88389db90c340cc13f50c99ac0f0c0b3e01f
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-09 11:08:30 -0400

    add test class stub

--- Test.Integration/ControllerTests/SkillsControllerTests/GetAllSkillsShould.cs
+++ Test.Integration/ControllerTests/SkillsControllerTests/GetAllSkillsShould.cs
@@ -1,7 +1,31 @@
+using System.Net.Http;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test.Integration.TestHelpers;
+
 namespace Test.Integration.ControllerTests.SkillsControllerTests
 {
+    [TestClass]
     public class GetAllSkillsShould
     {
-        
+        private TestServer _server;
+        private HttpClient _client;
+        private TestObjectCreator _testObjectCreator;
+        private int _languageId;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            (_server, _client) = new TestSetupHelper().GetTestServerAndClient();
+            _testObjectCreator = new TestObjectCreator(_client);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            var _ = _client.DeleteAsync($"{ControllerRouteEnum.LANGUAGES}/{_languageId}").Result;
+            _client.Dispose();
+            _server.Dispose();
+        }
     }
 }
