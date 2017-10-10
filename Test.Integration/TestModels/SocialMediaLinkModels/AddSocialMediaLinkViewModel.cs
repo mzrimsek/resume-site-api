12c62d4b2864bcf439f3f893b532bcb2ac234eef
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-10 12:49:02 -0400

    added social media test models

---
+++ Test.Integration/TestModels/SocialMediaLinkModels/AddSocialMediaLinkViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Test.Integration.TestModels.SocialMediaLinkModels
+{
+    public class AddSocialMediaLinkViewModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string Url { get; set; }
+    }
+}
---
+++ Test.Integration/TestModels/SocialMediaLinkModels/UpdateSocialMediaLinkViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Core.Interfaces;
+
+namespace Test.Integration.TestModels.SocialMediaLinkModels
+{
+    public class UpdateSocialMediaLinkViewModel : Web.Models.SocialMediaLinkModels.AddSocialMediaLinkViewModel, IHasId
+    {
+        [Required]
+        public int Id { get; set; }
+    }
+}
