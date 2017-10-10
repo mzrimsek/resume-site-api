38249f9cc9371da1db7c26b1b62cd62daa8ffb3f
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-10 13:40:59 -0400

    add tests for adding a social media link

---
+++ Test.Integration/ControllerTests/SocialMediaLinksControllerTests/AddSocialMediaLinkShould.cs
@@ -0,0 +1,84 @@
+﻿using System.Net;
+using System.Net.Http;
+using FluentAssertions;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Test.Integration.TestHelpers;
+using Test.Integration.TestModels.SocialMediaLinkModels;
+
+namespace Test.Integration.ControllerTests.SocialMediaLinksControllerTests
+{
+    [TestClass]
+    public class AddSocialMediaLinkShould
+    {
+        private TestServer _server;
+        private HttpClient _client;
+        private int _socialMediaLinkId;
+
+        [TestInitialize]
+        public void SetUp()
+        {
+            (_server, _client) = new TestSetupHelper().GetTestServerAndClient();
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+            var _ = _client.DeleteAsync($"{ControllerRouteEnum.SocialMediaLinks}/{_socialMediaLinkId}").Result;
+            _client.Dispose();
+            _server.Dispose();
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeCreated_WhenGivenValidModel()
+        {
+            var model = TestObjectGetter.GetAddSocialMediaLinkViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.SocialMediaLinks}", requestContent).Result;
+            _socialMediaLinkId = RequestHelper.GetObjectFromResponseContent<SocialMediaLinkViewModel>(response).Id;
+
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+        }
+
+        [TestMethod]
+        public void ReturnStatusCodeBadRequest_WhenGivenInvalidModel()
+        {
+            var model = TestObjectGetter.GetAddSocialMediaLinkViewModel(null);
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.SocialMediaLinks}", requestContent).Result;
+
+            response.StatusCode.Should().Be(HttpStatusCode.BadRequest);
+        }
+
+        [TestMethod]
+        public void ReturnCorrectViewModel()
+        {
+            var model = TestObjectGetter.GetAddSocialMediaLinkViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.SocialMediaLinks}", requestContent).Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<SocialMediaLinkViewModel>(response);
+            _socialMediaLinkId = serializedContent.Id;
+
+            var isCorrectViewModel = AssertHelper.AreTestSocialMediaLinkViewModelsEqual(model, serializedContent);
+            isCorrectViewModel.Should().BeTrue();
+        }
+
+        [TestMethod]
+        public void SaveCorrectViewModel()
+        {
+            var model = TestObjectGetter.GetAddSocialMediaLinkViewModel();
+            var requestContent = RequestHelper.GetRequestContentFromObject(model);
+
+            var response = _client.PostAsync($"{ControllerRouteEnum.SocialMediaLinks}", requestContent).Result;
+            _socialMediaLinkId = RequestHelper.GetObjectFromResponseContent<SocialMediaLinkViewModel>(response).Id;
+            response = _client.GetAsync($"{ControllerRouteEnum.SocialMediaLinks}/{_socialMediaLinkId}").Result;
+            var serializedContent = RequestHelper.GetObjectFromResponseContent<SocialMediaLinkViewModel>(response);
+            
+            var isCorrectViewModel = AssertHelper.AreTestSocialMediaLinkViewModelsEqual(model, serializedContent);
+            isCorrectViewModel.Should().BeTrue();
+        }
+    }
+}
