e18a7a51e85c4242b4f7200adca7f40ff9a9174e
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-10 13:38:25 -0400

    forgot to add viewmodel mapping profile

---
+++ Web/MapperProfiles/SocialMediaLinkProfile.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using Core.Models;
+using Web.Models.SocialMediaLinkModels;
+
+namespace Web.MapperProfiles
+{
+    public class SocialMediaLinkProfile : Profile
+    {
+        public SocialMediaLinkProfile()
+        {
+            CreateMap<SocialMediaLinkViewModel, SocialMediaLinkDomainModel>();
+            CreateMap<AddSocialMediaLinkViewModel, SocialMediaLinkDomainModel>();
+            CreateMap<SocialMediaLinkDomainModel, SocialMediaLinkViewModel>();
+            CreateMap<UpdateSocialMediaLinkViewModel, SocialMediaLinkViewModel>();
+        }
+    }
+}
