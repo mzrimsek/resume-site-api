5f51368f9ef1699112fe15acae597daf7cc63589
Mike Zrimsek <mikezrimsek@gmail.com>
2017-10-11 09:58:51 -0400

    implemented projects controller

---
+++ Web/Controllers/ProjectsController.cs
@@ -0,0 +1,54 @@
+﻿using System.Threading.Tasks;
+using AutoMapper;
+using Core.Interfaces.RepositoryInterfaces;
+using Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using Web.ActionFilters;
+using Web.Helpers;
+using Web.Models.ProjectModels;
+
+namespace Web.Controllers
+{
+    [Route("api/[controller]")]
+    public class ProjectsController : Controller
+    {
+        private readonly ControllerRequestHelper<ProjectDomainModel, ProjectViewModel> _controllerRequestHelper;
+        public ProjectsController(IProjectRepository projectRepository, IMapper mapper)
+        {
+            _controllerRequestHelper = new ControllerRequestHelper<ProjectDomainModel, ProjectViewModel>(projectRepository, mapper);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAllProjects()
+        {
+            return await _controllerRequestHelper.GetAll();
+        }
+
+        [HttpGet("{id}", Name = "GetProject")]
+        public async Task<IActionResult> GetProject(int id)
+        {
+            return await _controllerRequestHelper.GetById(id);
+        }
+
+        [HttpPost]
+        [ModelStateValidation]
+        public async Task<IActionResult> AddProject([FromBody] AddProjectViewModel entity)
+        {
+            return await _controllerRequestHelper.Add(entity, "GetProject");
+        }
+
+        [HttpPut("{id}")]
+        [ModelStateValidation]
+        [ModelHasCorrectId]
+        public async Task<IActionResult> UpdateProject(int id, [FromBody] UpdateProjectViewModel entity)
+        {
+            return await _controllerRequestHelper.Update(id, entity);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteProject(int id)
+        {
+            return await _controllerRequestHelper.Delete(id);
+        }
+    }
+}
